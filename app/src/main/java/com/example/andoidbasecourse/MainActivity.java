ba48dcdc9f602120348b3c976ce50c1ea65223c1
niww <niww@ya.ru>
2019-11-02 23:17:10 +0300

    Start course Create theme

--- app/src/main/java/com/example/andoidbasecourse/MainActivity.java
+++ app/src/main/java/com/example/andoidbasecourse/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andoidbasecourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setDarkMode(View view) {
+//        super.setTheme(R.style.AppDarkTheme);
+        super.setTheme(R.style.AppTheme);
+
+
     }
 }
