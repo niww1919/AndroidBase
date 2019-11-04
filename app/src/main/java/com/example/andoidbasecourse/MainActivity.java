763e4bd54d86e153dc7180bf05bc70fb3b32858d
niww <niww@ya.ru>
2019-11-04 11:29:20 +0300

    Home work 1

--- app/src/main/java/com/example/andoidbasecourse/MainActivity.java
+++ app/src/main/java/com/example/andoidbasecourse/MainActivity.java
@@ -2,20 +2,42 @@ package com.example.andoidbasecourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch aSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+//        getDelegate().setLocalNightMode(R.style.AppDarkTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aSwitch = findViewById(R.id.switch1);
+
+
     }
 
     public void setDarkMode(View view) {
-//        super.setTheme(R.style.AppDarkTheme);
-        super.setTheme(R.style.AppTheme);
 
+
+//        super.setTheme(R.style.AppDarkTheme);
+
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//        setTheme(R.style.AppDarkTheme);
+//        MainActivity.this.recreate();
+        if (aSwitch.isChecked()) {
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+//        MainActivity.this.setTheme(R.style.AppDarkTheme);
+//        MainActivity.this.recreate();
 
     }
 }
