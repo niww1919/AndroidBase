252bdb7946a3f2961b6d98a9aee7a073af15dc1c
niww <niww@ya.ru>
2019-12-01 21:21:39 +0300

    Home work 2-1 Work with action bar

--- app/src/main/java/com/example/andoidbasecourse/MainActivity.java
+++ app/src/main/java/com/example/andoidbasecourse/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.andoidbasecourse;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,10 +30,12 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
 
         List<Weather> weatherOFWeekDays = new ArrayList<>();
         weatherOFWeekDays.add(new Weather("+10", "Monday", "1"));
@@ -46,14 +52,11 @@ public class MainActivity extends BaseActivity {
 //        weatherOfCity.add(new Weather("+9", "London", new Date(System.currentTimeMillis())));
 
 
-
-
         RecyclerView recyclerView = findViewById(R.id.rv);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         WeatherAdapter adapter = new WeatherAdapter(this, weatherOFWeekDays);
         recyclerView.setAdapter(adapter);
-
 
 
         WeatherOfCityAdapter adapter2 = new WeatherOfCityAdapter(this, weatherOfCity);
@@ -63,12 +66,11 @@ public class MainActivity extends BaseActivity {
         rv.setAdapter(adapter2);
 
 
-
     }
 
     public void intentSettingsOfApp(View view) {
         Intent intent = new Intent(MainActivity.this, SettingsOfApp.class);
-        startActivityForResult(intent,SETTING_CODE);
+        startActivityForResult(intent, SETTING_CODE);
     }
 
     @Override
@@ -80,11 +82,37 @@ public class MainActivity extends BaseActivity {
 
 
         if (requestCode != REQUEST_CODE) {
-            return ;
+            return;
         }
-        ((TextView)findViewById(R.id.currentCityName)).setText(data.getStringExtra("City"));
+        ((TextView) findViewById(R.id.currentCityName)).setText(data.getStringExtra("City"));
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Button button = findViewById(R.id.activityButtonOfSettings);
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.itemButtonOfSettings) {
+            button.setVisibility(View.VISIBLE);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
